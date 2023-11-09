97fe0d3a127c9668d01aaa9188214f88c37767aa
ryleerobinson <89955410+ryleerobinson@users.noreply.github.com>
2023-11-08 19:39:16 -0700

    updated

--- ica11.js
+++ ica11.js
@@ -1,35 +1,47 @@
-const btn = document.querySelector("button");
-btn.addEventListener("click", () => {
-  displayMessage("Your inbox is almost full — delete some emails", "warning");
-  displayMessage("Brian: Hi there, how are you today?", "chat");
-});
+console.log("is this linked correctly");
 
-function displayMessage(msgText, msgType) {
-  const body = document.body;
+const x = 2;
+const y = 2;
 
-  const panel = document.createElement("div");
-  panel.setAttribute("class", "msgBox");
-  body.appendChild(panel);
-
-  const msg = document.createElement("p");
-  msg.textContent = msgText;
-  panel.appendChild(msg);
-
-  const closeBtn = document.createElement("button");
-  closeBtn.textContent = "x";
-  panel.appendChild(closeBtn);
-
-  closeBtn.addEventListener("click", () => panel.parentNode.removeChild(panel));
-
-  if (msgType === "warning") {
-    msg.style.backgroundImage = "url(icons/warning.png)";
-    panel.style.backgroundColor = "red";
-  } else if (msgType === "chat") {
-    msg.style.backgroundImage = "url(icons/chat.png)";
-    panel.style.backgroundColor = "aqua";
-  } else {
-    msg.style.paddingLeft = "20px";
-  }
+if (x === y) {
+    console.log("success");
+}
+else {
+    console.log("fail");
 }
 
-displayMessage();
+const btn = document.querySelector("button");
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+document.addEventListener("DOMContentLoaded", () => {
+    canvas.width = document.documentElement.clientWidth;
+    canvas.height = document.documentElement.clientHeight;
+});
+
+function random(number) {
+    return Math.floor(Math.random () * number);
+}
+
+function draw() {
+    //console.log("test");
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    for (let i = 0; i < 100; i++) {
+        ctx.beginPath();
+        let red = random (255);
+        let green = random(255);
+        let blue = random(255);
+        let color = "rgba("+red+","+green+","+blue+",0.5)"
+        console.log(color);
+        ctx.fillStyle = color;
+        ctx.arc(
+            random(canvas.width),
+            random(canvas.height),
+            random(150), 0, 2 * Math.PI,
+        );
+        ctx.fill();
+    }
+}
+
+
+btn.addEventListener("click", draw);
