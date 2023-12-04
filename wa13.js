8069a81507945114df936b38fec2ea003874d929
ryleerobinson <89955410+ryleerobinson@users.noreply.github.com>
2023-12-03 20:41:34 -0700

    Update wa13.js

--- wa13.js
+++ wa13.js
@@ -1,194 +1,112 @@
+const employees = [
+    {
+      "firstName": "Sam",
+      "department": "Tech",
+      "designation": "Manager",
+      "salary": 40000,
+      "raiseEligible": true
+    },
+    {
+      "firstName": "Mary",
+     "department": "Finance",
+      "designation": "Trainee",
+      "salary": 18500,
+      "raiseEligible": true
+    },
+    {
+      "firstName": "Bill",
+      "department": "HR",
+      "designation": "Executive",
+      "salary": 21200,
+      "raiseEligible": false
+    },
+    ];
+    console.log("Question 1");
+    console.log(employees);
 
-console.log("Question 1")
-const q1e1 = {
-    "name": "Sam",
-    "department": "Tech",
-    "designation": "Manager",
-    "salary": 40000,
-    "raiseEligible": true,
-}
-const q1e2 = {
-    "name": "Mary",
-    "department": "Finance",
-    "designation": "Trainee",
-    "salary": 18500,
-    "raiseEligible": true,
-}
-const q1e3 = {
-    "name": "Bill",
-    "department": "HR",
-    "designation": "Executive",
-    "salary": 21200,
-    "raiseEligible": false,
-}
-console.log(q1e1,q1e2,q1e3)
+
+const companyDetails = {
+    companyName: "Tech Stars",
+    website: "www.techstars.site",
+    employees: employees}
+
+    console.log("Question 2");
+    console.log(companyDetails);
 
 
-console.log("Question 2")
-const q2comp = {
-    "companyName": "Tech Stars",
-    "website": "www.techstars.site",
-    "employees": [
+const staff = [
         {
-            "name": "Sam",
+            "firstName": "Sam",
             "department": "Tech",
             "designation": "Manager",
             "salary": 40000,
-            "raiseEligible": true,
-        },
-        {
-            "name": "Mary",
-            "department": "Finance",
+            "raiseEligible": true
+          },
+          {
+            "firstName": "Mary",
+           "department": "Finance",
             "designation": "Trainee",
             "salary": 18500,
-            "raiseEligible": true,
-        },
-        {
-            "name": "Bill",
+            "raiseEligible": true
+          },
+          {
+            "firstName": "Bill",
             "department": "HR",
             "designation": "Executive",
             "salary": 21200,
-            "raiseEligible": false,
-        },
-    ],
-}
-console.log(q2comp)
-
-console.log("Question 3")
-const q3comp = {
-    "companyName": "Tech Stars",
-    "website": "www.techstars.site",
-    "employees": [
+            "raiseEligible": false
+          },
         {
-            "name": "Sam",
-            "department": "Tech",
-            "designation": "Manager",
-            "salary": 40000,
-            "raiseEligible": true,
-        },
-        {
-            "name": "Mary",
-            "department": "Finance",
-            "designation": "Trainee",
-            "salary": 18500,
-            "raiseEligible": true,
-        },
-        {
-            "name": "Bill",
-            "department": "HR",
-            "designation": "Executive",
-            "salary": 21200,
-            "raiseEligible": false,
-        },
-        {
-            "name": "Anna",
+            "firstName": "Anna",
             "department": "Tech",
             "designation": "Executive",
             "salary": 25600,
-            "raiseEligible": false,
-        },
-    ],
-}
-console.log(q3comp)
+            "raiseEligible": false
+        }
+    ];
+
+    console.log("Question 3");
+    console.log(staff);
 
 
-console.log("Question 4")
-var q4totalSal = 0;
-for(i=0; i < q3comp.employees.length; i++){
-    // console.log("salary of", q3comp.employees[i].name, ":", q3comp.employees[i].salary)
-    q4totalSal += q3comp.employees[i].salary;
-}
-console.log("Total salary: ", q4totalSal)
+function totalSalary(staff){
+        let totalSalary = 0;
 
-
-console.log("Question 5")
-const q5comp = {
-    "companyName": "Tech Stars",
-    "website": "www.techstars.site",
-    "employees": [
-        {
-            "name": "Sam",
-            "department": "Tech",
-            "designation": "Manager",
-            "salary": 40000,
-            "raiseEligible": true,
-        },
-        {
-            "name": "Mary",
-            "department": "Finance",
-            "designation": "Trainee",
-            "salary": 18500,
-            "raiseEligible": true,
-        },
-        {
-            "name": "Bill",
-            "department": "HR",
-            "designation": "Executive",
-            "salary": 21200,
-            "raiseEligible": false,
-        },
-        {
-            "name": "Anna",
-            "department": "Tech",
-            "designation": "Executive",
-            "salary": 25600,
-            "raiseEligible": false,
-        },
-    ],
-}
-for(i=0; i < q5comp.employees.length; i++){
-    if(q5comp.employees[i].raiseEligible){
-        // console.log(q5comp.employees[i].name, "gets a raise!");
-        q5comp.employees[i].salary += q5comp.employees[i].salary * .1;
-        q5comp.employees[i].raiseEligible = false;
+    for(employee of staff){
+        totalSalary += employee.salary;
     }
-    // console.log(q5comp.employees[i].name, ":", q5comp.employees[i].salary);
-}
-console.log(q5comp)
+   return totalSalary;
+   }
+    console.log("Question 4");
+    console.log(totalSalary(staff));
 
 
-console.log("Question 6")
-const q6comp = {
-    "companyName": "Tech Stars",
-    "website": "www.techstars.site",
-    "employees": [
-        {
-            "name": "Sam",
-            "department": "Tech",
-            "designation": "Manager",
-            "salary": 44000,
-            "raiseEligible": true,
-        },
-        {
-            "name": "Mary",
-            "department": "Finance",
-            "designation": "Trainee",
-            "salary": 20350,
-            "raiseEligible": true,
-        },
-        {
-            "name": "Bill",
-            "department": "HR",
-            "designation": "Executive",
-            "salary": 21200,
-            "raiseEligible": false,
-        },
-        {
-            "name": "Anna",
-            "department": "Tech",
-            "designation": "Executive",
-            "salary": 25600,
-            "raiseEligible": false,
-        },
-    ],
-}
-q6wfh = ['Anna', 'Sam'];
-for(i=0; i < q6comp.employees.length; i++){
-    if(q6comp.employees[i].name == q6wfh[0] || q6comp.employees[i].name == q6wfh[1]){
-        q6comp.employees[i].wfh = true;
+function updateSalary() {
+    staff.forEach((e) => {
+      if (e.raiseEligible) {
+        e.salary = e.salary * 1.1;
+        e.raiseEligible = false;
+      };
+    });
+  }
+
+  updateSalary();
+
+    console.log("Question 5");
+    console.log(staff);
+
+
+const workFromHome = ["Anna", "Sam"];
+
+function workHome(){
+    staff.forEach((e) => {
+        if (workFromHome.includes(e.firstName)) {
+        e.workFromHome = true;
     }
-    else {
-        q6comp.employees[i].wfh = false;
-    }
-}
-console.log(q6comp)
+    })
+  }
+
+  workHome();
+
+    console.log("Question 6");
+    console.log(staff);
